d88c2b8d1104099a63053fc50506a68d8e08fb2f
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-07-10 16:35:05 -0300

    Controller de mensajes

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,26 +8,6 @@ use Illuminate\Http\Request;
 class MessageController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -35,53 +15,13 @@ class MessageController extends Controller
      */
     public function store(Request $request)
     {
-        Message::create($request->all());
+        $agent = $request->server->get('HTTP_USER_AGENT');
+        $matches = preg_match('/Firefox|Chrom|Safari|Edge|Opera|IE|Trident/i', $agent);
 
-        return redirect()->route('home');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Message $message)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Message $message)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Message $message)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Message $message)
-    {
-        //
+        if ($matches) {
+            return Message::create($request->all());
+        } else {
+            return response(['message' => 'Not allowed agent'], 400);
+        }
     }
 }
